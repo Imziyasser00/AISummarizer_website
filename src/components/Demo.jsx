557d6475bbed3ba4ea91f2bc60d4e2468e615d62
Yasser Imzi <87503470+Imziyasser00@users.noreply.github.com>
2024-02-11 15:41:02 +0100

    step

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { FaLink } from "react-icons/fa";
-
+import { useLazyGetSummaryQuery } from '../services/article';
 
 
 const Demo = () => {
@@ -9,9 +9,22 @@ const Demo = () => {
     summary:'',
   })
 
+  const [getSummary, {error, isFetching }] = useLazyGetSummaryQuery();
+
   const handleSubmit = async (e) =>{
-    
+    e.preventDefault();
+    console.log(article.url);
+    const { data } = await getSummary({ articleUrl : article.url})
+    if(data?.summary) {
+      const newArticle = { ...article, summary:data.summary}
+      setArticle(newArticle)
+
+      console.log(newArticle);
+    }
+  
   }
+
+
 
   return (
     <section className='mt-16 w-screen max-w-xl'>
@@ -22,13 +35,14 @@ const Demo = () => {
           <div className='relative flex justify-center items-center'>
               <FaLink className='absolute left-0 my-2 ml-3 w-5 text-gray-500'/>
               <input 
-              type='url'
-              placeholder='Enter a URL'
-              value={article.url}
-              onChange={(e)=>{setArticle({ ...article, url: e.target.url })}}
-              required
-              className='bg-white w-[600px] border-0 h-12 w-full px-12 shadow-lg shadow-blue-400/30'
-            />
+                  type='url'
+                  placeholder='Enter a URL'
+                  value={article.url}
+                  onChange={(e) => {setArticle({ ...article, url: e.target.value })}}
+                  required
+                  className='bg-white w-[700px] border-0 h-12 w-full px-12 shadow-lg shadow-blue-400/30'
+                />
+
           </div>
           
           <button className='text-[22px] mt-12 bg-amber-500 hover:bg-orange-600 px-6 py-1 text-white transition ease-in-out '
